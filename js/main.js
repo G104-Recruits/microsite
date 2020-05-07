c8caf93a63b5be9b3460435460628810abf88d96
PedroDuarte536 <pedro.dld536@gmail.com>
2020-05-07 13:26:08 +0100

    Photos Update

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
   var docsPath = 'docs/';
-  var docs = ['teste.doc'];
+  var docs = [{name: 'teste.doc', date: '07/05/2019'}];
 
   var row = $($('div#docs tbody tr')[0]);
   $('div#docs tbody tr').remove();
 
   docs.forEach(doc => {
-    row.find('td[data-label="Documento"]').text(doc);
-    row.find('td[data-label="Download"] a')[0].href =   docsPath + doc;
+    row.find('td[data-label="Documento"]').text(doc.name);
+    row.find('td[data-label="Última Atualização"]').text(doc.date);
+    row.find('td[data-label="Download"] a')[0].href =   docsPath + doc.name;
     
     row.appendTo($('div#docs tbody'));
   })
